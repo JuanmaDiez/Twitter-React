ebea218a0569f749a3e4cfcd5452b5aa290611b6
Juan Manuel Diez <jm10yap@gmail.com>
2022-10-18 15:22:38 -0300

    register

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -4,7 +4,6 @@ const publicController = require("../controllers/publicController");
 const passport = require("passport");
 
 publicRouter.get("/login", publicController.login);
-publicRouter.post("/register", publicController.create);
 publicRouter.post(
   "/login",
   passport.authenticate("local", {
@@ -13,6 +12,7 @@ publicRouter.post(
   })
 );
 publicRouter.get("/register", publicController.register);
+publicRouter.post("/register", publicController.create);
 publicRouter.get("/logout", publicController.logOut);
 
 module.exports = publicRouter;
