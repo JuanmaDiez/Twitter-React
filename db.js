e12d7fec3ce979e01131c4780f408f18b03cef15
Juan Manuel Diez <jm10yap@gmail.com>
2022-10-18 09:25:28 -0300

    .env and formidable

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const DATABASE_NAME = process.env.DATABASE_NAME;
 
-mongoose.connect("mongodb://127.0.0.1:27017/db-twitter");
+mongoose.connect(`mongodb://127.0.0.1:27017/${DATABASE_NAME}`);
 mongoose.connection
   .once("open", () => console.log("Conexion con la base de datos establecida"))
   .on("error", () => console.log("Ha habido un error"));
