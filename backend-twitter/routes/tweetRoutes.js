0f25c8cfeb70bcc9f71f17a17523d8a50d11accc
Juan Manuel Diez <jm10yap@gmail.com>
2022-11-03 18:35:35 -0300

    axios call functioning and slight changes in routes

--- backend-twitter/routes/tweetRoutes.js
+++ backend-twitter/routes/tweetRoutes.js
@@ -4,25 +4,12 @@ const tweetController = require("../controllers/tweetController");
 const { expressjwt: checkJwt } = require("express-jwt");
 const JWT_STRING_SECRETO = process.env.JWT_STRING_SECRETO;
 
-tweetRouter.get(
-  "/",
-  checkJwt({ secret: JWT_STRING_SECRETO, algorithms: ["HS256"] }),
-  tweetController.index
+tweetRouter.use(
+  checkJwt({ secret: JWT_STRING_SECRETO, algorithms: ["HS256"] })
 );
-tweetRouter.post(
-  "/",
-  checkJwt({ secret: JWT_STRING_SECRETO, algorithms: ["HS256"] }),
-  tweetController.store
-);
-tweetRouter.delete(
-  "/profile/:tweetId",
-  checkJwt({ secret: JWT_STRING_SECRETO, algorithms: ["HS256"] }),
-  tweetController.destroy
-);
-tweetRouter.patch(
-  "/:tweetId",
-  checkJwt({ secret: JWT_STRING_SECRETO, algorithms: ["HS256"] }),
-  tweetController.update
-);
+tweetRouter.get("/", tweetController.index);
+tweetRouter.post("/", tweetController.store);
+tweetRouter.delete("/profile/:tweetId", tweetController.destroy);
+tweetRouter.patch("/:tweetId", tweetController.update);
 
 module.exports = tweetRouter;
--- frontend-twitter/src/pages/Home.jsx
+++ frontend-twitter/src/pages/Home.jsx
@@ -5,12 +5,13 @@ import axios from "axios";
 function Home() {
   const user = useSelector((state) => state.user);
   console.log(user);
+  console.log(user.token);
   useEffect(() => {
     const getData = async () => {
       const response = await axios({
         url: "http://localhost:8000",
         method: "GET",
-        headers: { Authorizaton: `Bearer ${user.token}` },
+        headers: { Authorization: `Bearer ${user.token}` },
       });
       console.log(response.data);
     };
